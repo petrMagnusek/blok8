7bc2e2069de793320829773efa82c9bbb0247a2f
petr.magnusek@t-mobile.cz <petr.magnusek@t-mobile.cz>
2023-09-02 17:23:06 +0200

    Javascript included

---
+++ user-greetings.js
@@ -0,0 +1,7 @@
+"use strict";
+let fname = window.prompt("Enter your first name:");
+let sname = window.prompt("Enter your surname:");
+let age = window.prompt("Enter your age:");
+console.log(`Name is: ${fname} ${sname}`)
+console.log(`Your age is: ${age}`)
+alert(`Hello ${fname} ${sname}. Your age is: ${age}`)
